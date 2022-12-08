886dc876ee743aed03dd704bde81025c39cda711
Melih Özmen <melihozmen9@gmail.com>
2022-12-08 16:54:58 +0300

    tasarım
    Tasarım ve Sliderlar baglandı.

---
+++ BodyMassIndex/Controller/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  BodyMassIndex
+//
+//  Created by Kullanici on 8.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        var height = String(format: "%.2f", sender.value)
+       print(height)
+        heightLabel.text = "\(height)m"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        var weight = String(format: "%.0f", sender.value)
+        print(weight)
+        weightLabel.text = "\(weight)kg"
+        
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        var BMI = weight / pow(height, 2)
+        print(BMI)
+    }
+    
+}
+
